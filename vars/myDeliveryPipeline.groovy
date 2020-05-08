0c1f2d369db83258f902f6a72963f686639ce956
Philippe Schild <pschild@users.noreply.github.com>
2020-05-08 08:54:20 +0200

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -1,13 +1,7 @@
 def call(Map pipelineParams) {
-
-    pipeline {
-        agent any
-        stages {
-            stage('checkout git') {
-                steps {
-                    git changelog: false, url: pipelineParams.repoUrl
-                }
-            }
+    stage('checkout git') {
+        steps {
+            git changelog: false, url: pipelineParams.repoUrl
         }
     }
 }
