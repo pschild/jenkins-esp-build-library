52098b6a2f4a70b80ef7cf97a00e78fa3b1fcc2b
Philippe Schild <pschild@users.noreply.github.com>
2020-05-09 19:33:55 +0200

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -9,7 +9,9 @@ def call(Map pipelineParams) {
         environment {
             //FIRMWARE_NAME="${sh(script:'echo ${pipelineParams.repoUrl} | grep -P "([^/]+$)" -o | sed "s/.git//g"', returnStdout: true).trim()}"
             FIRMWARE_NAME=sh(
-                script: 'git --no-pager show -s --format=\'%ae\'',
+                script: '''
+                    git --no-pager show -s --format=\'%ae\'
+                ''',
                 returnStdout: true
             ).trim()
             FIRMWARE_VERSION="v${BUILD_NUMBER}-${sh(script:'git rev-parse HEAD', returnStdout: true).trim().take(7)}"
