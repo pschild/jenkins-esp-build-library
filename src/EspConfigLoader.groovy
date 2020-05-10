9d3bedd42d5d3f383142b6d9518ade4c5ad3e319
Philippe Schild <pschild@users.noreply.github.com>
2020-05-10 12:11:23 +0200

    Create EspConfigLoader.groovy

---
+++ src/EspConfigLoader.groovy
@@ -0,0 +1,17 @@
+class EspConfigLoader {
+    private int x;
+    private int y;
+
+    public Sample(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Sample{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
