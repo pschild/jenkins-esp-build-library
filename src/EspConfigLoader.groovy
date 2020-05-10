a47e0638cee9cad284ab32ad25e704eb7de1de7e
Philippe Schild <pschild@users.noreply.github.com>
2020-05-10 12:20:17 +0200

    Update EspConfigLoader.groovy

--- src/EspConfigLoader.groovy
+++ src/EspConfigLoader.groovy
@@ -3,13 +3,14 @@ import groovy.json.JsonSlurper
 class EspConfigLoader {
     private JsonSlurper slurper = new JsonSlurper()
     private String path
+    Script script
 
     public EspConfigLoader(String path) {
         this.path = path
     }
     
     public parseIt() {
-        def jsonResponse = this.slurper.parseText(libraryResource(this.path))
+        def jsonResponse = this.slurper.parseText(script.libraryResource(this.path))
         def result = []
         jsonResponse.each {
             result << it.chipId + "|" + it.pioEnv
